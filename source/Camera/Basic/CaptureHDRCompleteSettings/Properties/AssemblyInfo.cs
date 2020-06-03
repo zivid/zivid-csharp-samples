c3b046b91cd85654807952a7980d0cc212ac2dbf
AzureAD\SatjaSivcev <satja.sivcev@zivid.com>
2020-06-03 15:39:02 +0200

    Update Copyright info

--- source/Camera/Basic/CaptureHDRCompleteSettings/Properties/AssemblyInfo.cs
+++ source/Camera/Basic/CaptureHDRCompleteSettings/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("CaptureHDRCompleteSettings")]
-[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyCopyright("Copyright 2015-2020 (C) Zivid AS")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
